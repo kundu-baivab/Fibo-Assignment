0fe88d3a9d6e3e83c7b08c05abdc27d089a6e3fd
Baivab Kundu <baivabkundu@gmail.com>
2024-05-15 10:04:12 +0530

    some additions

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -34,46 +34,50 @@ const Menu = () => {
       className="fixed bottom-0 w-full text-white"
       style={{ backgroundColor: "#282828" }}
     >
-      <div className="py-4">
-        <ul className="flex justify-center">
+      <div className="py-3">
+        <ul className="flex justify-between mx-2">
           <li className="mx-4">
-            <Link to="/">
+            <Link to="/" className="flex flex-col">
               <img
                 onClick={() => handleOptionClick(1)}
                 src={images[selectedOption].opt1}
                 className="w-10 h-10"
                 alt="Option 1"
               />
+              <p style={{color:selectedOption==1?"orange":"white"}}>Page 1</p>
             </Link>
           </li>
           <li className="mx-4">
-            <Link to="/page2">
+            <Link to="/page2" className="flex flex-col">
               <img
                 onClick={() => handleOptionClick(2)}
                 src={images[selectedOption].opt2}
                 className="w-10 h-10"
                 alt="Option 2"
               />
+              <p style={{color:selectedOption==2?"orange":"white"}}>Page 2</p>
             </Link>
           </li>
           <li className="mx-4">
-            <Link to="/page3">
+            <Link to="/page3" className="flex flex-col">
               <img
                 onClick={() => handleOptionClick(3)}
                 src={images[selectedOption].opt3}
                 className="w-10 h-10"
                 alt="Option 3"
               />
+              <p style={{color:selectedOption==3?"orange":"white"}}>Page 3</p>
             </Link>
           </li>
           <li className="mx-4">
-            <Link to="/page4">
+            <Link to="/page4" className="flex flex-col">
               <img
                 onClick={() => handleOptionClick(4)}
                 src={images[selectedOption].opt4}
                 className="w-10 h-10"
                 alt="Option 4"
               />
+              <p style={{color:selectedOption==4?"orange":"white"}}>Page 4</p>
             </Link>
           </li>
         </ul>
